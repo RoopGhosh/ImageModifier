b7ecb7e8af3b15d78f010090e7e3b6d168e2929f
Roop <rghosh@infinio.com>
2016-08-01 23:59:35 -0400

    Pushing everything

--- ImageFilter/src/main/java/com/ImageFilter/controller/FileUploadController.java
+++ ImageFilter/src/main/java/com/ImageFilter/controller/FileUploadController.java
@@ -1,6 +1,7 @@
 package com.ImageFilter.controller;
 
 import com.ImageFilter.jpg2GreyScale.Jpg2GreyScale;
+import com.ImageFilter.jpg2GreyScale.Operations;
 import com.ImageFilter.model.FileBucket;
 import com.ImageFilter.model.MultiFileBucket;
 import com.ImageFilter.util.FileValidator;
@@ -14,13 +15,16 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @Controller
 public class FileUploadController {
@@ -63,19 +67,31 @@ public class FileUploadController {
 	}
 
 	@RequestMapping(value="/singleUpload", method = RequestMethod.POST)
-	public String singleFileUpload(@Valid FileBucket fileBucket, BindingResult result, ModelMap model) throws IOException {
-
+	public String singleFileUpload(@Valid FileBucket fileBucket, @RequestParam String operation, BindingResult result, ModelMap model) throws IOException, ExecutionException {
+		System.out.println(Operations.values());
 		if (result.hasErrors()) {
 			System.out.println("validation errors");
 			return "singleFileUploader";
 		} else {			
 			System.out.println("Fetching file");
 			MultipartFile multipartFile = fileBucket.getFile();
-
 			//Now do something with file...
 			String filepath = UPLOAD_LOCATION + fileBucket.getFile().getOriginalFilename();
 			FileCopyUtils.copy(fileBucket.getFile().getBytes(), new File(filepath));
-			String proccesedPath = jpg2GreyScale.getimage(filepath);
+			String proccesedPath=null;
+			if(!Arrays.asList(Operations.values().toString()).contains(operation)){
+				throw new ExecutionException("operation not part of operationENUM",null);
+			}
+			switch (operation.toUpperCase()){
+				case "GREYSCALE":
+					proccesedPath = jpg2GreyScale.getimage(filepath);
+				case "COLOR":
+					System.out.println("Not Implemented");
+					//proccesedPath = jpg2GreyScale.getimage(filepath);
+				case "UNDEFINED":
+					System.out.println("Something bad happened in the switch case. ");
+			}
+
 			String fileName = multipartFile.getOriginalFilename();
 			model.addAttribute("fileName", proccesedPath);
 			return "success";
---
+++ ImageFilter/src/main/java/com/ImageFilter/jpg2GreyScale/Operations.java
@@ -0,0 +1,11 @@
+package com.ImageFilter.jpg2GreyScale;
+
+/**
+ * Created by roopghosh on 8/1/16.
+ */
+public enum Operations {
+    GREYSCALE,
+    COLOR,
+    UNDEFINED;
+
+}
